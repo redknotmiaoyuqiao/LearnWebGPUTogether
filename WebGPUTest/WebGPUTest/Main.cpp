36ea54bf0f57193e29acf03943abc5437a6e2e5b
redknot <redknot@126.com>
2023-04-18 23:49:14 +0800

    create window

--- WebGPUTest/WebGPUTest/Main.cpp
+++ WebGPUTest/WebGPUTest/Main.cpp
@@ -1,4 +1,29 @@
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
 int main(int argc,char **argv)
 {
+    if (!glfwInit()) {
+        printf("Could not initialize GLFW!\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
+    if (!window) {
+        printf("Could not open window!\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
